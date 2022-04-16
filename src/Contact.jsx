3fedb98b10400a26bef7838484aea110dc6f6788
creativoma <ma.marianoalvarez@gmail.com>
2022-04-16 17:57:23 +0200

    Second commit

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,14 +1,18 @@
+import img1 from './images/phone-icon.png'
+import img2 from './images/phone-icon.png'
+
+
 export default function Contact(props) {
     return (
         <div className="contact-card">
             <img src={props.img}/>
             <h3>{props.name}</h3>
             <div className="info-group">
-                <img src="./src/images/phone-icon.png" />
+                <img src={img1} />
                 <p>{props.phone}</p>
             </div>
             <div className="info-group">
-                <img src="./src/images/mail-icon.png" />
+                <img src={img2} />
                 <p>{props.email}</p>
             </div>
         </div>
